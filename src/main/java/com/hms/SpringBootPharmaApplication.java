e04782685ae8d2af6f4381eee648a42829473642
vishant pawar <vishant9612@gmail.com>
2025-10-05 11:16:49 +0530

    demo

--- src/main/java/com/hms/SpringBootPharmaApplication.java
+++ src/main/java/com/hms/SpringBootPharmaApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootPharmaApplication {
-
+ 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootPharmaApplication.class, args);
 	}
